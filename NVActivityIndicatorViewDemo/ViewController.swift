c628d0233b66728123d124bddcfad0307b9426c0
Nguyen Vinh <ninjaprox@gmail.com>
2015-07-23 13:27:33 +0400

    Ball clip rotate multiple demo

--- NVActivityIndicatorViewDemo/ViewController.swift
+++ NVActivityIndicatorViewDemo/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
             .BallPulse,
             .BallGridPulse,
             .BallClipRotate,
-            .BallClipRotatePulse]
+            .BallClipRotatePulse,
+            .BallClipRotateMultiple]
         let cols = 6
         let rows = 6
         let cellWidth = Int(self.view.frame.width / CGFloat(cols))
