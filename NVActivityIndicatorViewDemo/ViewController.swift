b4269bed6638faebd020b83ab26412607034ebc7
Nguyen Vinh <ninjaprox@gmail.com>
2015-07-23 14:33:54 +0400

    Ball zig zag deflect animation demo

--- NVActivityIndicatorViewDemo/ViewController.swift
+++ NVActivityIndicatorViewDemo/ViewController.swift
@@ -22,7 +22,8 @@ class ViewController: UIViewController {
             .BallClipRotatePulse,
             .BallClipRotateMultiple,
             .BallRotate,
-            .BallZigZag]
+            .BallZigZag,
+            .BallZigZagDeflect]
         let cols = 6
         let rows = 6
         let cellWidth = Int(self.view.frame.width / CGFloat(cols))
