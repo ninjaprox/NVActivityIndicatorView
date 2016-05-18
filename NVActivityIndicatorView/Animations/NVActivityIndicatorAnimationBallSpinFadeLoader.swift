106d3f6c9c34cf56376e50389e6a7de50c5e1160
Nguyen Vinh <ninjaprox@gmail.com>
2016-05-18 17:55:38 +0700

    Fix content out of bound in BallSpinFadeLoader animation

--- NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallSpinFadeLoader.swift
+++ NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallSpinFadeLoader.swift
@@ -57,11 +57,11 @@ class NVActivityIndicatorAnimationBallSpinFadeLoader: NVActivityIndicatorAnimati
     }
     
     func circleAt(angle angle: CGFloat, size: CGFloat, origin: CGPoint, containerSize: CGSize, color: UIColor) -> CALayer {
-        let radius = containerSize.width / 2
+        let radius = containerSize.width / 2 - size / 2
         let circle = NVActivityIndicatorShape.Circle.createLayerWith(size: CGSize(width: size, height: size), color: color)
         let frame = CGRect(
-            x: origin.x + radius * (cos(angle) + 1) - size / 2,
-            y: origin.y + radius * (sin(angle) + 1) - size / 2,
+            x: origin.x + radius * (cos(angle) + 1),
+            y: origin.y + radius * (sin(angle) + 1),
             width: size,
             height: size)
         
