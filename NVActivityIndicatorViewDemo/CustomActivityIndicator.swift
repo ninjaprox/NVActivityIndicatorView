1d9e2181c70ac057d6c4dea2e6986f3d846f6e26
Basem Emara <basememara@gmail.com>
2016-06-12 19:07:16 -0400

    Updated custom animation demo.

--- NVActivityIndicatorViewDemo/CustomActivityIndicator.swift
+++ NVActivityIndicatorViewDemo/CustomActivityIndicator.swift
@@ -9,142 +9,48 @@
 import UIKit
 
 class CustomActivityIndicator: NVActivityIndicatorAnimationDelegate {
-    let duration: CFTimeInterval = 1.9
-    let distanceIncrement: CGFloat = 25
-    let satelliteIncrement: CGFloat = 5
-    let coreIncrement: CGFloat = 25
-    
-    var satelliteSize: CGFloat = 0
-    var coreSize: CGFloat = 0
-    
-    func setUpAnimationInLayer(layer: CALayer, size: CGSize, color: UIColor) {
-        satelliteSize = size.width / 15 + satelliteIncrement
-        coreSize = size.width / 5 + coreIncrement
-        
-        ring1InLayer(layer, size: size, color: color)
-        ring2InLayer(layer, size: size, color: color)
-        coreInLayer(layer, size: size, color: color)
-        satelliteInLayer(layer, size: size, color: color)
-    }
-    
-    func ring1InLayer(layer: CALayer, size: CGSize, color: UIColor) {
-        // Scale animation
-        let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        
-        scaleAnimation.keyTimes = [0, 0.45, 0.45, 1]
-        scaleAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        scaleAnimation.values = [0, 0, 1.3, 2]
-        scaleAnimation.duration = duration
-        
-        // Opacity animation
-        let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
-        let timingFunction = CAMediaTimingFunction(controlPoints: 0.19, 1, 0.22, 1)
-        
-        opacityAnimation.keyTimes = [0, 0.45, 1]
-        scaleAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear), timingFunction]
-        opacityAnimation.values = [0.8, 0.8, 0]
-        opacityAnimation.duration = duration
-        
-        // Animation
-        let animation = CAAnimationGroup()
-        
-        animation.animations = [scaleAnimation, opacityAnimation]
-        animation.duration = duration
-        animation.repeatCount = HUGE
-        animation.removedOnCompletion = false
 
-        // Draw circle
-        let circle = NVActivityIndicatorShape.Circle.createLayerWith(size: CGSize(width: coreSize, height: coreSize), color: color)
-        let frame = CGRectMake((layer.bounds.size.width - coreSize) / 2,
-                               (layer.bounds.size.height - coreSize) / 2,
-                               coreSize,
-                               coreSize)
+    func setUpAnimationInLayer(layer: CALayer, size: CGSize, color: UIColor) {
+        let lineSize = size.width / 9
+        let x = (layer.bounds.size.width - size.width) / 2
+        let y = (layer.bounds.size.height - size.height) / 2
+        let duration: [CFTimeInterval] = [4.3, 2.5, 1.7, 3.1]
+        let beginTime = CACurrentMediaTime()
+
+        let values = [0, 0.7, 0.4, 0.05, 0.95, 0.3, 0.9, 0.4, 0.15, 0.18, 0.75, 0.01]
         
-        circle.frame = frame
-        circle.addAnimation(animation, forKey: "animation")
-        layer.addSublayer(circle)
-    }
-    
-    func ring2InLayer(layer: CALayer, size: CGSize, color: UIColor) {
-        // Scale animation
-        let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        
-        scaleAnimation.keyTimes = [0, 0.55, 0.55, 1]
-        scaleAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        scaleAnimation.values = [0, 0, 1.3, 2.1]
-        scaleAnimation.duration = duration
-        
-        // Opacity animation
-        let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
-        let timingFunction = CAMediaTimingFunction(controlPoints: 0.19, 1, 0.22, 1)
-        
-        opacityAnimation.keyTimes = [0, 0.55, 0.65, 1]
-        scaleAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear), timingFunction]
-        opacityAnimation.values = [0.7, 0.7, 0, 0]
-        opacityAnimation.duration = duration
-        
-        // Animation
-        let animation = CAAnimationGroup()
-        
-        animation.animations = [scaleAnimation, opacityAnimation]
-        animation.duration = duration
-        animation.repeatCount = HUGE
-        animation.removedOnCompletion = false
-        
-        // Draw circle
-        let circle = NVActivityIndicatorShape.Circle.createLayerWith(size: CGSize(width: coreSize, height: coreSize), color: color)
-        let frame = CGRectMake((layer.bounds.size.width - coreSize) / 2,
-                               (layer.bounds.size.height - coreSize) / 2,
-                               coreSize,
-                               coreSize)
-        
-        circle.frame = frame
-        circle.addAnimation(animation, forKey: "animation")
-        layer.addSublayer(circle)
-    }
-    
-    func coreInLayer(layer: CALayer, size: CGSize, color: UIColor) {
-        let inTimingFunction = CAMediaTimingFunction(controlPoints: 0.7, 0, 1, 0.5)
-        let outTimingFunction = CAMediaTimingFunction(controlPoints: 0, 0.7, 0.5, 1)
-        let standByTimingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        
-        // Scale animation
-        let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        
-        scaleAnimation.keyTimes = [0, 0.45, 0.55, 1]
-        scaleAnimation.timingFunctions = [inTimingFunction, standByTimingFunction, outTimingFunction];
-        scaleAnimation.values = [1, 1.3, 1.3, 1]
-        scaleAnimation.duration = duration
-        scaleAnimation.repeatCount = HUGE
-        scaleAnimation.removedOnCompletion = false
-        
-        // Draw circle
-        let circle = NVActivityIndicatorShape.Circle.createLayerWith(size: CGSize(width: coreSize, height: coreSize), color: color)
-        let frame = CGRectMake((layer.bounds.size.width - coreSize) / 2,
-                               (layer.bounds.size.height - coreSize) / 2,
-                               coreSize,
-                               coreSize)
-        
-        circle.frame = frame
-        circle.addAnimation(scaleAnimation, forKey: "animation")
-        layer.addSublayer(circle)
-    }
-    
-    func satelliteInLayer(layer: CALayer, size: CGSize, color: UIColor) {
-        // Rotate animation
-        let rotateAnimation = CAKeyframeAnimation(keyPath: "position")
-        
-        rotateAnimation.path = UIBezierPath(arcCenter: CGPoint(x: CGRectGetMidX(layer.bounds), y: CGRectGetMidY(layer.bounds)), radius: (size.width - satelliteSize) / 2 + distanceIncrement, startAngle: 3 * CGFloat(M_PI) * 0.5, endAngle: 3 * CGFloat(M_PI) * 0.5 + 4 * CGFloat(M_PI), clockwise: true).CGPath
-        rotateAnimation.duration = duration * 2
-        rotateAnimation.repeatCount = HUGE
-        rotateAnimation.removedOnCompletion = false
-        
-        // Draw circle
-        let circle = NVActivityIndicatorShape.Circle.createLayerWith(size: CGSize(width: satelliteSize, height: satelliteSize), color: color)
-        let frame = CGRectMake(0, 0, satelliteSize, satelliteSize)
-        
-        circle.frame = frame
-        circle.addAnimation(rotateAnimation, forKey: "animation")
-        layer.addSublayer(circle)
+        // Draw lines
+        for i in 0 ..< 4 {
+            let animation = CAKeyframeAnimation()
+            animation.keyPath = "path"
+            animation.additive = true
+            animation.values = []
+
+            for j in 0 ..< values.count {
+                var point = CGPointZero
+                let htFactor = values[j]
+                let ht = size.height * CGFloat(htFactor)
+                point.y = point.y + (size.height - ht)
+                let path = UIBezierPath(rect: CGRect(origin: point, size: CGSize(width: lineSize, height: ht)))
+
+                animation.values?.append(path.CGPath)
+            }
+
+            let line = NVActivityIndicatorShape.Line.createLayerWith(size: CGSize(width: lineSize, height: size.height), color: color)
+
+            let frame = CGRect(x: x + lineSize * 2 * CGFloat(i),
+                y: y,
+                width: lineSize,
+                height: size.height)
+
+            animation.duration = duration[i]
+            animation.repeatCount = HUGE
+            animation.removedOnCompletion = false
+
+            animation.beginTime = beginTime
+            line.frame = frame
+            line.addAnimation(animation, forKey: "animation")
+            layer.addSublayer(line)
+        }
     }
 }
