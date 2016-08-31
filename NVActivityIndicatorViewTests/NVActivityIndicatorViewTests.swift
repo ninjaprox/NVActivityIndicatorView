43277c2721b8eea748be653bb672c58ad5837e64
Nguyen Vinh <ninjaprox@gmail.com>
2016-08-31 17:06:05 +0800

    Add test to pass Travis config

---
+++ NVActivityIndicatorViewTests/NVActivityIndicatorViewTests.swift
@@ -0,0 +1,35 @@
+//
+//  NVActivityIndicatorViewTests.swift
+//  NVActivityIndicatorViewTests
+//
+//  Created by Nguyen Vinh on 8/31/16.
+//  Copyright © 2016 Nguyen Vinh. All rights reserved.
+//
+
+import XCTest
+
+class NVActivityIndicatorViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
