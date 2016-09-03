a867c7c42acbfc8ca91d0315ce71f7fb5d5fdd2e
Nguyen Vinh <ninjaprox@gmail.com>
2016-09-03 18:49:08 +0800

    Fix NVActivityIndicatorType.allTypes test

--- NVActivityIndicatorView/NVActivityIndicatorView.swift
+++ NVActivityIndicatorView/NVActivityIndicatorView.swift
@@ -237,7 +237,7 @@ public enum NVActivityIndicatorType: Int {
      */
     case AudioEqualizer
     
-    static let allTypes = (Blank.rawValue ... Orbit.rawValue).map{ NVActivityIndicatorType(rawValue: $0)! }
+    static let allTypes = (Blank.rawValue ... AudioEqualizer.rawValue).map{ NVActivityIndicatorType(rawValue: $0)! }
 
     func animation() -> NVActivityIndicatorAnimationDelegate {
         switch self {
