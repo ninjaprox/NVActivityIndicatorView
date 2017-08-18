e2152ac8ca1338a10023d2857f1ca510391a9f9e
Parita Shah <pshah@pshah-sierra.nyoffice.hypr.com>
2017-08-18 16:35:40 -0400

    updating activity indicator loader circles from 3 to 10

--- NVActivityIndicatorView/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallBeat.swift
+++ NVActivityIndicatorView/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallBeat.swift
@@ -31,44 +31,44 @@ class NVActivityIndicatorAnimationBallBeat: NVActivityIndicatorAnimationDelegate
 
     func setUpAnimation(in layer: CALayer, size: CGSize, color: UIColor) {
         let circleSpacing: CGFloat = 2
-        let circleSize = (size.width - circleSpacing * 2) / 3
-        let x = (layer.bounds.size.width - size.width) / 2
+        let circleSize = (size.width - circleSpacing * 2) / 2
+        let x = (layer.bounds.size.width - size.width) / 2 - 50
         let y = (layer.bounds.size.height - circleSize) / 2
         let duration: CFTimeInterval = 0.7
         let beginTime = CACurrentMediaTime()
-        let beginTimes = [0.35, 0, 0.35]
-
+        let beginTimes = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        
         // Scale animation
         let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-
+        
         scaleAnimation.keyTimes = [0, 0.5, 1]
         scaleAnimation.values = [1, 0.75, 1]
         scaleAnimation.duration = duration
-
+        
         // Opacity animation
         let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
-
-        opacityAnimation.keyTimes = [0, 0.5, 1]
-        opacityAnimation.values = [1, 0.2, 1]
+        
+        opacityAnimation.keyTimes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] //[0, 0.5, 1]
+        opacityAnimation.values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] //[1, 0.2, 1]
         opacityAnimation.duration = duration
-
+        
         // Aniamtion
         let animation = CAAnimationGroup()
-
+        
         animation.animations = [scaleAnimation, opacityAnimation]
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
         animation.duration = duration
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-
+        
         // Draw circles
-        for i in 0 ..< 3 {
+        for i in 0 ..< 10 {
             let circle = NVActivityIndicatorShape.circle.layerWith(size: CGSize(width: circleSize, height: circleSize), color: color)
             let frame = CGRect(x: x + circleSize * CGFloat(i) + circleSpacing * CGFloat(i),
                                y: y,
                                width: circleSize,
                                height: circleSize)
-
+            
             animation.beginTime = beginTime + beginTimes[i]
             circle.frame = frame
             circle.add(animation, forKey: "animation")
