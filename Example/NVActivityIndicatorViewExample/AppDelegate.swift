4574fbed6d302320280acca2aaf468d28496437b
“Alexander <alexsanderskywork@gmail.com>
2018-09-15 19:05:53 +0400

    Updated example to Swift 4.2

--- Example/NVActivityIndicatorViewExample/AppDelegate.swift
+++ Example/NVActivityIndicatorViewExample/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
