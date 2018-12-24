a7c5538da559c80b9ab8fe4b09902d17972c130f
Vinh Nguyen <ninjaprox@gmail.com>
2018-12-25 00:38:26 +0700

    Version 4.5.0

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,4 +4,5 @@ type="${1:-patch}"
 
 fastlane run increment_version_number bump_type:"$type" xcodeproj:"Source/NVActivityIndicatorView.xcodeproj"
 fastlane run version_bump_podspec bump_type:"$type" path:"NVActivityIndicatorView.podspec"
+pod install --project-directory=Example
 
