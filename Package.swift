f34b7c815e6cc39d1cdb6cf15ae93c5cf3aa4f03
Rafael Francisco <rafaelfrancisco6@gmail.com>
2019-09-10 10:17:48 +0100

    Added SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NVActivityIndicatorView",
+    products: [
+        .library(name: "NVActivityIndicatorView",  targets: ["NVActivityIndicatorView"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "NVActivityIndicatorView", path: "Source/NVActivityIndicatorView")
+    ]
+)
+
