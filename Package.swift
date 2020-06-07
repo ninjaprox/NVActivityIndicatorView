d541f928cf78943bf27552d9dc8c36c0a50f86a1
Vinh Nguyen <ninjaprox@gmail.com>
2020-06-07 15:25:30 +0700

    Update swift-tools-version to 5.0

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NVActivityIndicatorView",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9)
+    ],
     products: [
         .library(name: "NVActivityIndicatorView", targets: ["NVActivityIndicatorView"])
     ],
-    dependencies: [],
     targets: [
         .target(name: "NVActivityIndicatorView", path: "Source/NVActivityIndicatorView")
     ]
