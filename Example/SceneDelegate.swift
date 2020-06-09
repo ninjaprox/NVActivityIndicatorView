3179d957595e421bcebca5b8ef0466c2e2527bc9
Vinh Nguyen <ninjaprox@gmail.com>
2020-06-09 21:08:27 +0700

    Unify .swiftlint.yml

--- Example/SceneDelegate.swift
+++ Example/SceneDelegate.swift
@@ -35,7 +35,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard (scene as? UIWindowScene) != nil else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
