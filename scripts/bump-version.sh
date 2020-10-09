4b72c830fe10a103afa9a990130600a57c7e9aed
Vinh Nguyen <ninjaprox@gmail.com>
2020-10-09 19:41:32 +0700

    Auto bump build number

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -2,6 +2,7 @@
 
 type="${1:-bump}"
 
+fastlane run increment_build_number
 fastlane run increment_version_number bump_type:"$type"
 fastlane run version_bump_podspec bump_type:"$type" path:"NVActivityIndicatorView.podspec"
 
